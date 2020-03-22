5d2f674df04df0a4a062885ee7a3896801c2b7f9
marcus quigley <marcusquigley@gmail.com>
2020-03-22 10:41:13 -0400

    fractional knapsack. My attempt

--- greedy/python/alg_toolbox_lectures.py
+++ greedy/python/alg_toolbox_lectures.py
@@ -1,9 +1,7 @@
 def largest_number(numbers):
     maxindex = -1
     currentindex = 0
-    #print(type(numbers))
     n = len(numbers)
-    #results = []
     while currentindex < n:
         maxvalue = -1
         for i in range(currentindex, n):
@@ -14,9 +12,47 @@ def largest_number(numbers):
         numbers[maxindex] = numbers[currentindex]
         numbers[currentindex] = temp
         currentindex += 1
-        #numbers.pop(maxindex)
-        #results.append(str(maxvalue))
     return ''.join(numbers)
     
-    
+def fractional_knapsack(bagcapacity, weights, values):
+    n = len(weights)
+    #voverw = [0] * n 
+    result = 0
+   
+    usedweights = set()
+    while result < bagcapacity or len(usedweights) < n:
+        maxvalue = -1
+        maxindex = -1
+        for index in range(n):
+            w = weights[index]
+            if w not in usedweights: #this should be set difference if 2 not in usedweights
+                v = int(w /values[index])
+                if v > maxvalue:
+                    maxvalue = v
+                    maxindex = index
+        #bagcapacity -= weights[maxindex]
+        if (maxindex == -1):
+            break
+        weight = weights[maxindex]
+        value = values[maxindex]
+        
+        if value < (bagcapacity - result):
+            result += value
+            usedweights.add(weight)
+        else:
+            val = value // (bagcapacity - result)  
+            result += val
+            usedweights.add(weight) #??
+            #break
+
+
+    return result
+            
+
+
+            
+
+
+
+
 
--- tests/test_greedy_alg_toolbox_lectures.py
+++ tests/test_greedy_alg_toolbox_lectures.py
@@ -2,9 +2,20 @@ import unittest
 from greedy.python import alg_toolbox_lectures
 
 class Test_greedy_algorithms(unittest.TestCase):
+    def test_fractional_knapsack(self):
+        weights = [20,18,14]
+        values = [4,3,2]
+        capacity = 7
+        expected = 7
+        actual = alg_toolbox_lectures.fractional_knapsack(capacity, weights, values)
+        self.assertEqual(expected, actual)
     
     def test_largest_number(self):
         numbers = [3,9,5,9,7,1]
         expected = "997531"
         actual = alg_toolbox_lectures.largest_number(numbers)
         self.assertEqual(expected, actual)
+
+   
+
+        
